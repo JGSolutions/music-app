c5afc059ac4e52a7f0fadc827af1628bef537e98
Jerry Gagliano <jerrygag@gmail.com>
2021-04-10 09:33:48 -0400

    added a done button

--- src/app/mixcloud-callback/mixcloud-callback.module.ts
+++ src/app/mixcloud-callback/mixcloud-callback.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MixcloudCallbackComponent } from './mixcloud-callback.component';
+import { AngularMaterialModule } from 'src/angular-material.module';
 
 
 
@@ -9,6 +10,7 @@ import { MixcloudCallbackComponent } from './mixcloud-callback.component';
   declarations: [MixcloudCallbackComponent],
   imports: [
     CommonModule,
+    AngularMaterialModule,
     RouterModule.forChild([
       { path: '', component: MixcloudCallbackComponent },
     ]),
