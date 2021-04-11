929e38a542a1a41d5d4f8b36d4bebc168f7ebb96
Jerry Gagliano <jerrygag@gmail.com>
2021-04-10 21:55:32 -0400

    fixed the connection token

--- src/app/spotify-callback/spotify-callback.component.ts
+++ src/app/spotify-callback/spotify-callback.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { SpotifyAuthorization } from 'functions/sdk/spotify.sdk';
 import { isEmpty } from 'lodash';
-import { filter, map, switchMap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { MusicConnectedService } from '../services/music-connected.services';
 
@@ -11,9 +12,12 @@ import { MusicConnectedService } from '../services/music-connected.services';
   templateUrl: './spotify-callback.component.html',
   styleUrls: ['./spotify-callback.component.scss']
 })
-export class SpotifyCallbackComponent implements OnInit {
+export class SpotifyCallbackComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<boolean>();
 
-  constructor(private route: ActivatedRoute, private connectedServices: MusicConnectedService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private connectedServices: MusicConnectedService) { }
 
   ngOnInit(): void {
     SpotifyAuthorization.config(
@@ -24,18 +28,18 @@ export class SpotifyCallbackComponent implements OnInit {
 
     this.route.queryParams.pipe(
       filter((params: Params) => !isEmpty(params)),
-      map(async (params: Params) => {
-        const res: any = await SpotifyAuthorization.createAccessTokenUrl(params.code);
+      takeUntil(this.destroy$)
+    ).subscribe(async(params: any) => {
+      const res: any = await SpotifyAuthorization.createAccessTokenUrl(params.code);
 
-        console.log(res.data.access_token);
-        return res.data.access_token;
-      })
-    ).subscribe((code: any) => {
-      console.log(code)
       this.connectedServices.connectService({
-        token: code,
+        token: res.data.access_token,
       }, 'spotify');
     });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
   }
 
 }
