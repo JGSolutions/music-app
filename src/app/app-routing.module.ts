77bf4aaba84eb3b4a3db72099b7489ea6c46f508
Jerry Gagliano <jerrygag@gmail.com>
2021-04-17 22:14:33 -0400

    added auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,35 @@
 import { NgModule } from '@angular/core';
+import { redirectUnauthorizedTo, AngularFireAuthGuard } from '@angular/fire/auth-guard';
 import { RouterModule, Routes } from '@angular/router';
 
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/platform-settings',
+    pathMatch: 'full'
+  },
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginModule)
   },
   {
     path: 'platform-settings',
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
     loadChildren: () => import('./platform-settings/platform-settings.module').then( m => m.PlatformSettingsModule)
   },
   {
     path: 'mixcloud-callback',
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
     loadChildren: () => import('./mixcloud-callback/mixcloud-callback.module').then( m => m.MixcloudCallbackModule)
   },
   {
     path: 'spotify-callback',
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
     loadChildren: () => import('./spotify-callback/spotify-callback.module').then( m => m.SpotifyCallbackModule)
   },
 ];
