40d167bb3b1e7919c34fb5bee35241367415ddc9
Jerry Gagliano <jerrygag@gmail.com>
2021-04-18 12:30:22 -0400

    added main layout side nav structure

--- src/app/app-player/app-player.module.ts
+++ src/app/app-player/app-player.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { AppPlayerRoutingModule } from './app-player-routing.module';
 import { MusicConnectedService } from '../services/music-connected.services';
 import { ApiService } from '../services/api.service';
+import { AngularMaterialModule } from 'src/angular-material.module';
 
 @NgModule({
   declarations: [AppPlayerComponent],
   imports: [
     CommonModule,
     RouterModule,
+    AngularMaterialModule,
     AppPlayerRoutingModule,
   ],
   providers: [MusicConnectedService, ApiService],
