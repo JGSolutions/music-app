ea15c87c6ed073130279a48c0adf36235774f30c
Jerry Gagliano <jerrygag@gmail.com>
2021-04-22 07:07:53 -0400

    displaying info and some styling

--- src/app/shared/components/artist-item/artist-item.component.ts
+++ src/app/shared/components/artist-item/artist-item.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IArtists } from 'functions/src/models/IArtists.types';
+import { Pictures } from 'functions/src/models/IPictures.types';
+import { Observable, ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -11,11 +14,30 @@ export class ArtistItemComponent implements OnInit {
   @Input()
   artistName!: string;
 
-  @Input()
-  artistDetails!: IArtists[];
+  @Input() set artistDetails(value: IArtists[]) {
+    this._artistDetails$.next(value);
+  }
+
+  public avatar$!: Observable<Pictures>;
+  public platforms$!: Observable<string[]>;
+
+  private _artistDetails$ = new ReplaySubject(1);
+
   constructor() { }
 
   ngOnInit(): void {
+    this.avatar$ = this._artistDetails$.pipe(
+      map((artistDetails: any) => {
+        const randomIdx = Math.floor(Math.random() * 1);
+        return artistDetails[randomIdx].pictures;
+      })
+    );
+
+    this.platforms$ = this._artistDetails$.pipe(
+      map((artistDetails: any) => {
+        return artistDetails.map((artist: any) => artist.platform)
+      })
+    );
   }
 
 }
