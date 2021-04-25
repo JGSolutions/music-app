9ca84d5e0401d81c8c113facc7c6f31c6190ebe0
Jerry Gagliano <jerrygag@gmail.com>
2021-04-24 21:29:59 -0400

    to pass body url as post

--- functions/src/modules/artist.ts
+++ functions/src/modules/artist.ts
@@ -2,7 +2,7 @@
 import { Response, Request } from "express";
 import { adminFirebase } from "./fb";
 // import { flatten, keys, reduce } from "lodash";
-// import { MixcloudSDK } from "../../sdk/mixcloud.sdk";
+import { MixcloudSDK } from "../../sdk/mixcloud.sdk";
 import { IPlatformTypes } from "../../sdk/IPlatforms.types";
 // import { SpotifySDK } from "../../sdk/spotify.sdk";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -13,42 +13,38 @@ const db = adminFirebase.firestore();
 export const artist = async (request: Request, response: Response) => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const authorized = request.headers["authorization"]!;
-  const requestBody = request.body;
+  const requestBody = JSON.parse(request.body);
 
   if (!requestBody) {
-    response.status(403).send("Invalid");
+    response.status(403).send("Invalid Body Request");
   }
 
   if (!authorized) {
-    response.status(401).send("Invlaid authenticated");
+    response.status(401).send("Invalid authenticated");
   }
 
-  try {
-    await adminFirebase.auth().getUser(authorized);
-    const connectedServicesRef = await db.collection("connectedServices").doc(authorized).get();
-    const connectedServices = connectedServicesRef.data() as FirebaseFirestore.DocumentData;
+  // try {
+  await adminFirebase.auth().getUser(authorized);
+  const connectedServicesRef = await db.collection("connectedServices").doc(authorized).get();
+  const connectedServices = connectedServicesRef.data() as FirebaseFirestore.DocumentData;
 
-    // const platformKeys = keys(connectedServicesRef.data());
-    // const pData: unknown[] = [];
+  requestBody.forEach(async (key: any) => {
+    switch (key.type) {
+      case IPlatformTypes.mixcloud:
+        MixcloudSDK.initialize(connectedServices[key.type].token);
+        // pData.push(MixcloudSDK.following());
+        break;
+      case IPlatformTypes.spotify:
+        // SpotifySDK.initialize(connectedServices[key].token);
+        // pData.push(SpotifySDK.following("artist"));
+        break;
+    }
+  });
 
-    requestBody.forEach(async (key: any) => {
-      switch (key.type) {
-        case IPlatformTypes.mixcloud:
-          console.log(key);
-          // MixcloudSDK.initialize(connectedServices[key].token);
-          // pData.push(MixcloudSDK.following());
-          break;
-        case IPlatformTypes.spotify:
-          // SpotifySDK.initialize(connectedServices[key].token);
-          // pData.push(SpotifySDK.following("artist"));
-          break;
-      }
-    });
-
-    response.status(200).send(connectedServices);
-  } catch (err) {
-    response.status(401).send("Not authenticated");
-  }
+  response.status(200).send(connectedServices);
+  // } catch (err) {
+  //   response.status(401).send(err);
+  // }
 
   // Promise.all(pData).then((promiseData: any[]) => {
   // });
