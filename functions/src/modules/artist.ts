2c796f0fb5eee3d34ac9cc3e5786fca6fa5839e7
Jerry Gagliano <jerrygag@gmail.com>
2021-04-25 09:23:17 -0400

    fixed the try catch for auth and request body

--- functions/src/modules/artist.ts
+++ functions/src/modules/artist.ts
@@ -4,47 +4,57 @@ import { adminFirebase } from "./fb";
 // import { flatten, keys, reduce } from "lodash";
 import { MixcloudSDK } from "../../sdk/mixcloud.sdk";
 import { IPlatformTypes } from "../../sdk/IPlatforms.types";
-// import { SpotifySDK } from "../../sdk/spotify.sdk";
+import { SpotifySDK } from "../../sdk/spotify.sdk";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 
 const db = adminFirebase.firestore();
+
+interface ArtistBodyRequest {
+  type: string;
+  id: string;
+}
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const artist = async (request: Request, response: Response) => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const authorized = request.headers["authorization"]!;
-  const requestBody = JSON.parse(request.body);
+  let requestBody: ArtistBodyRequest[];
 
-  if (!requestBody) {
+  try {
+    requestBody = JSON.parse(request.body);
+  } catch (err) {
     response.status(403).send("Invalid Body Request");
+    return;
   }
 
   if (!authorized) {
     response.status(401).send("Invalid authenticated");
   }
 
-  // try {
-  await adminFirebase.auth().getUser(authorized);
+  try {
+    await adminFirebase.auth().getUser(authorized);
+  } catch (err) {
+    response.status(401).send(err);
+    return;
+  }
+
   const connectedServicesRef = await db.collection("connectedServices").doc(authorized).get();
   const connectedServices = connectedServicesRef.data() as FirebaseFirestore.DocumentData;
 
-  requestBody.forEach(async (key: any) => {
+  requestBody.forEach(async (key: ArtistBodyRequest) => {
     switch (key.type) {
       case IPlatformTypes.mixcloud:
         MixcloudSDK.initialize(connectedServices[key.type].token);
         // pData.push(MixcloudSDK.following());
         break;
       case IPlatformTypes.spotify:
-        // SpotifySDK.initialize(connectedServices[key].token);
+        SpotifySDK.initialize(connectedServices[key.type].token);
         // pData.push(SpotifySDK.following("artist"));
         break;
     }
   });
 
   response.status(200).send(connectedServices);
-  // } catch (err) {
-  //   response.status(401).send(err);
-  // }
 
   // Promise.all(pData).then((promiseData: any[]) => {
   // });
