6233ff5b6363b22ad0253bc13af57e5ce2542cff
Jerry Gagliano <jerrygag@gmail.com>
2021-05-15 07:26:34 -0400

    able to accept external url for endpoint

--- functions/src/modules/mixcloudAudio.ts
+++ functions/src/modules/mixcloudAudio.ts
@@ -8,6 +8,15 @@ import { getConnectServices } from "../utils/connect-services-firebase";
 export const mixcloudAudio = async (request: Request, response: Response) => {
   const authorized = request.headers["authorization"]!;
 
+  let requestBody: { externalUrl: string };
+
+  try {
+    requestBody = request.body;
+  } catch (err) {
+    response.status(403).send("Invalid Body Request");
+    return;
+  }
+
   if (!authorized) {
     response.status(401).send("Invalid authenticated");
   }
@@ -23,6 +32,6 @@ export const mixcloudAudio = async (request: Request, response: Response) => {
 
   MixcloudSDK.initialize(connectedServices[IPlatformTypes.mixcloud].token);
 
-  const { data: result } = await MixcloudSDK.audioStream("https://www.mixcloud.com/hernancattaneo/resident-episode-522-may-08-2021/");
+  const { data: result } = await MixcloudSDK.audioStream(requestBody.externalUrl);
   response.status(200).send({ url: result.url });
 };
