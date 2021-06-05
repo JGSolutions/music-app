9e4458b5f4646779348908a96120086504942178
Jerry Gagliano <jerrygag@gmail.com>
2021-06-05 19:00:08 -0400

    enabling dev api route

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -21,8 +21,8 @@ export const environment = {
     clientId: '9e85a20c69144326899c6fdae66167e7',
     secretApi: 'd412e35440c64c1b804123c3e295f5f8'
   },
-  restapiDomain: "https://music-restapi.web.app/api",
-  // restapiDomain: "http://localhost:5000/api"
+  // restapiDomain: "https://music-restapi.web.app/api",
+  restapiDomain: "http://localhost:5000/api"
 };
 
 /*
