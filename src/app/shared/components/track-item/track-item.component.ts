14deb630f9ef7e58269b38505509dd3a5acb532e
Jerry Gagliano <jerrygag@gmail.com>
2021-06-20 08:08:49 -0400

    created track item component

---
+++ src/app/shared/components/track-item/track-item.component.ts
@@ -0,0 +1,48 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { IPlatformTypes } from 'models/artist.types';
+import { IAvatar } from 'models/avatar.types';
+import { ISongTrackType } from 'models/song.types';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-track-item',
+  templateUrl: './track-item.component.html',
+  styleUrls: ['./track-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TrackItemComponent {
+  @Input()
+  name!: string;
+
+  @Input()
+  id!: string;
+
+  @Input()
+  avatar!: string;
+
+  @Input()
+  length!: number;
+
+  @Input()
+  dateCreated!: any;
+
+  @Input()
+  trackType!: ISongTrackType;
+
+  @Input()
+  platform!: IPlatformTypes;
+
+  @Input()
+  externalUrl!: string;
+
+  @Output() selectedSong = new EventEmitter<string>();
+
+  public avatar$!: Observable<IAvatar>;
+  public platforms$!: Observable<string[]>;
+  public trackTypes = ISongTrackType;
+
+  public selectedItem(): void {
+    this.selectedSong.emit(this.id);
+  }
+}
+
---
+++ src/app/shared/components/track-item/track-item.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TrackItemComponent } from './track-item.component';
+import { AngularMaterialModule } from 'src/angular-material.module';
+
+@NgModule({
+  declarations: [TrackItemComponent],
+  imports: [
+    CommonModule,
+    // AngularMaterialModule
+  ],
+  exports: [
+    TrackItemComponent
+  ]
+})
+export class TrackItemModule { }
