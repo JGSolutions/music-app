b4031cee74d40b12b4ff53e0b2567feb9a76e922
Jerry Gagliano <jerrygag@gmail.com>
2021-07-10 09:06:39 -0400

    fixed song item hover

--- src/app/shared/components/song-item/song-item.module.ts
+++ src/app/shared/components/song-item/song-item.module.ts
@@ -4,13 +4,15 @@ import { SongItemComponent } from './song-item.component';
 import { AngularMaterialModule } from 'src/angular-material.module';
 import { DateAgoPipe } from 'src/app/core/pipes/date-ago.pipe';
 import { MinuteSecondsModule } from 'src/app/core/pipes/minute-seconds.module';
+import { EqualizerIconModule } from '../equalizer-icon/equalizer-icon.module';
 
 @NgModule({
   declarations: [SongItemComponent, DateAgoPipe],
   imports: [
     CommonModule,
     MinuteSecondsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    EqualizerIconModule
   ],
   exports: [
     SongItemComponent
