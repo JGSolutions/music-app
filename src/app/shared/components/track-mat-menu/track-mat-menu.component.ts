50f459907fcd67165add73a76dd192929722be29
Jerry Gagliano <jerrygag@gmail.com>
2021-07-22 22:08:18 -0400

    create song list menu

---
+++ src/app/shared/components/track-mat-menu/track-mat-menu.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: "app-track-mat-menu",
+  templateUrl: "./track-mat-menu.component.html",
+  styleUrls: ["./track-mat-menu.component.scss"],
+})
+export class TrackMatMenuComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+}
