f68bbdad7afb01db29ec6b0b17900772ef37ddb4
Jerry Gagliano <jerrygag@gmail.com>
2021-07-29 19:24:15 -0400

    fixed typings

--- src/app/shared/components/playlist-item/playlist-item.component.ts
+++ src/app/shared/components/playlist-item/playlist-item.component.ts
@@ -1,24 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IAvatar } from 'models/avatar.types';
 import { Observable } from 'rxjs';
+import { ICoverImages } from 'src/app/core/stores/playlist/playlist.types';
 
 @Component({
   selector: 'app-playlist-item',
   templateUrl: './playlist-item.component.html',
   styleUrls: ['./playlist-item.component.scss']
 })
-export class PlaylistItemComponent implements OnInit {
+export class PlaylistItemComponent {
   @Input()
   playlistName!: string;
 
   @Input()
-  coverImages!: string[];
+  coverImages!: ICoverImages[];
 
   public avatar$!: Observable<IAvatar>;
   public platforms$!: Observable<string[]>;
-
-  ngOnInit(): void {
-
-  }
 }
 
