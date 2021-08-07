1ca66b2cd2a16e39492100418ca64271cca36788
Jerry Gagliano <jerrygag@gmail.com>
2021-08-07 11:40:59 -0400

    fixed types

--- src/app/playlist-details/playlist-details.module.ts
+++ src/app/playlist-details/playlist-details.module.ts
@@ -5,11 +5,13 @@ import { PlaylistDetailsComponent } from './playlist-details.component';
 import { AngularMaterialModule } from 'src/angular-material.module';
 import { NgxsModule } from '@ngxs/store';
 import { PlaylistState } from '../core/stores/playlist/playlist.state';
+import { ImageGridModule } from '../shared/components/image-grid/image-grid.module';
 @NgModule({
   declarations: [PlaylistDetailsComponent],
   imports: [
     CommonModule,
     AngularMaterialModule,
+    ImageGridModule,
     NgxsModule.forFeature([PlaylistState]),
     RouterModule.forChild([
       { path: '', component: PlaylistDetailsComponent },
