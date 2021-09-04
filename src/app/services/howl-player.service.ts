7e17c09874aa0eed7d493f3ac16c64881bd47692
Jerry Gagliano <jerrygag@gmail.com>
2021-09-03 21:02:04 -0400

    created souncloud bar

---
+++ src/app/services/howl-player.service.ts
@@ -0,0 +1,101 @@
+import { Injectable } from '@angular/core';
+import { Howl, Howler } from 'howler';
+import { ReplaySubject, Subject } from 'rxjs';
+
+@Injectable()
+export class HowlerPlayerService {
+  public howler = Howler;
+  public $onload: Subject<void>;
+  public $sliderProgress: ReplaySubject<number>;
+  public $currentTimer: ReplaySubject<number>;
+  public $rawDuration: ReplaySubject<number>;
+  public $isPlaying: ReplaySubject<boolean>;
+
+  private _sound!: Howl;
+  private _raf = 0;
+
+  constructor() {
+    this.$onload = new Subject();
+    this.$sliderProgress = new ReplaySubject(1);
+    this.$currentTimer = new ReplaySubject(1);
+    this.$rawDuration = new ReplaySubject(1);
+    this.$isPlaying = new ReplaySubject(1);
+  }
+
+  public initHowler(streamUrl: string): void {
+    if (this._sound) {
+      cancelAnimationFrame(this._raf);
+      this._sound.unload();
+    }
+
+    this._sound = new Howl({
+      src: [streamUrl],
+      html5: true,
+      autoplay: false,
+      preload: 'metadata',
+      volume: 1,
+      onload: () => {
+        this.$currentTimer.next(0);
+        this.$rawDuration.next(this._sound?.duration()!);
+        this.$isPlaying.next(this._sound?.playing());
+        this.$sliderProgress.next(0);
+        this.$onload.next();
+      },
+      onplay: () => {
+        this.$isPlaying.next(this._sound.playing());
+        this._raf = requestAnimationFrame(this._whilePlaying.bind(this));
+      }
+    })
+  }
+
+  public duration(): number {
+    if (this._sound) {
+      return this._sound.duration();
+    }
+
+    return 0;
+  }
+
+  public play(): void {
+    if (this._sound) {
+      this._sound.play();
+    }
+  }
+
+  public pause(): void {
+    if (this._sound) {
+      if (this._sound?.playing()) {
+        cancelAnimationFrame(this._raf);
+      }
+      this._sound.pause();
+      this.$isPlaying.next(this._sound.playing());
+    }
+  }
+
+  public stop(): void {
+    if (this._sound) {
+      this._sound.stop();
+      this.$isPlaying.next(this._sound.playing());
+    }
+  }
+
+  public isPlaying(): boolean {
+    if (this._sound) {
+      return this._sound.playing();
+    }
+
+    return false;
+  }
+
+  private _whilePlaying() {
+    const seek = this._sound?.seek() || 0 as number;
+    this.$currentTimer.next(seek as number);
+    this.$sliderProgress.next(seek as number);
+
+    this._raf = requestAnimationFrame(this._whilePlaying.bind(this));
+  }
+
+  public seek(per: number): void {
+    this._sound?.seek(per);
+  }
+}
---
+++ src/app/shared/components/soundcloud-bar/soundcloud-bar.component.ts
@@ -0,0 +1,108 @@
+import { Component, Input, ChangeDetectionStrategy, OnInit, OnDestroy, EventEmitter, Output } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { distinctUntilChanged, filter, map, switchMap, takeUntil, withLatestFrom } from 'rxjs/operators';
+import { isEmpty as _isEmpty } from "lodash";
+import { Select, Store } from '@ngxs/store';
+import { ICurrentTrack } from 'src/app/core/stores/songs/songs.types';
+import { UserState } from 'src/app/core/stores/user/user.state';
+import { IUserType } from 'src/app/core/stores/user/user.types';
+import { SongsState } from 'src/app/core/stores/songs/songs.state';
+import { AudioFileAction, LoadingPlayerAction, SetCurrentTrackPlayStatusAction } from 'src/app/core/stores/songs/songs.actions';
+import { HowlerPlayerService } from 'src/app/services/howl-player.service';
+
+@Component({
+  selector: 'app-player-bar',
+  templateUrl: './soundcloud-bar.component.html',
+  styleUrls: ['./soundcloud-bar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SoundcloudBarComponent implements OnInit, OnDestroy {
+  @Select(UserState.userState) user$!: Observable<IUserType>;
+  @Select(SongsState.audioFile) audioFile$!: Observable<string>;
+  @Select(SongsState.loading) loading$!: Observable<boolean>;
+
+  @Input() currentTrack$!: Observable<ICurrentTrack>;
+
+  @Output() trackReady = new EventEmitter<any>();
+  public playSongLoading$ = new Subject<boolean>();
+
+  private destroy$ = new Subject<boolean>();
+
+  constructor(public howlService: HowlerPlayerService, private store: Store) { }
+
+  ngOnInit() {
+    this.currentTrack$.pipe(
+      takeUntil(this.destroy$),
+      distinctUntilChanged((prev, curr) => prev.externalUrl === curr.externalUrl),
+      filter((streamUrl) => _isEmpty(streamUrl.audioFile)),
+      map((streamUrl) => streamUrl.externalUrl),
+      withLatestFrom(this.user$),
+      switchMap(([url, user]) => this.store.dispatch(new AudioFileAction(user.uid, url))),
+    ).subscribe();
+
+    this.audioFile$.pipe(
+      takeUntil(this.destroy$),
+      filter((audioFile) => !_isEmpty(audioFile)),
+      withLatestFrom(this.currentTrack$),
+    ).subscribe(([audioFile, currentTrack]) => {
+      this.howlService.initHowler(audioFile);
+      this.store.dispatch(new LoadingPlayerAction(true));
+      this.trackReady.emit(currentTrack);
+    });
+
+    this.howlService.$onload.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(() => {
+      this.store.dispatch([
+        new SetCurrentTrackPlayStatusAction(false),
+        new LoadingPlayerAction(false)
+      ]);
+    });
+
+    this.howlService.$isPlaying.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(() => {
+      this.playSongLoading$.next(false);
+      this.store.dispatch(new SetCurrentTrackPlayStatusAction(true));
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+
+    this.stop();
+  }
+
+  public play(): void {
+    this.playSongLoading$.next(true);
+    this.howlService.play();
+  }
+
+  public pause(): void {
+    this.howlService.pause();
+    this.store.dispatch(new SetCurrentTrackPlayStatusAction(false));
+  }
+
+  public stop(): void {
+    this.howlService.stop();
+  }
+
+  /**
+   * When user stops dragging then seek and continue playing
+   * @param evt
+   */
+  public sliderChange(evt: number) {
+    this.howlService.seek(evt);
+    this.howlService.play();
+  }
+
+  /**
+   * When user drags slider just pause the music file and update timer as user is dragging
+   */
+  public sliderInput(evt: number): void {
+    this.howlService.$currentTimer.next(evt)
+    this.howlService.pause();
+  }
+
+}
