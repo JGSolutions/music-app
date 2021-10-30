5ae9bfa0e0f4ad10b42cc3915918177a19fc04c7
Jerry Gagliano <jerrygag@gmail.com>
2021-10-30 13:34:08 -0400

    added button

--- src/app/shared/components/playlist-item/playlist-item.module.ts
+++ src/app/shared/components/playlist-item/playlist-item.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlaylistItemComponent } from './playlist-item.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [PlaylistItemComponent],
   imports: [
     CommonModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
     PlaylistItemComponent
